48c25fafd3000e80049d38c33de021cff9b9fe09
Orlando Verdin <git@orli.land>
2024-11-17 14:59:13 -0700

    add banner and grid layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import Banner from "./components/Banner";
+
 function App() {
-  return null;
+  return (
+    <div className="w-full min-h-screen bg-light bg-center bg-cover ">
+      <div className="min-h-screen bg-opacity-50 bg-neutral-600 backdrop-blur-lg bg-white-700/50">
+        <Banner />
+        <main className="lg:w-9/12 p-4 mx-auto  grid gap-6 lg:grid-cols-3 lg:grid-rows-3">
+          <header className="lg:col-span-full lg:row-span-1">navbar</header>
+          <section className="lg:col-span-2  lg:row-span-1">search</section>
+          <section className="lg:col-span-2  lg:row-span-1">cart list</section>
+          <section className="lg:col-start-3  lg:col-end-4 lg:row-start-2 lg:row-end-4">
+            details
+          </section>
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Banner.jsx
@@ -0,0 +1,12 @@
+const Banner = () => {
+  return (
+    <div className="text-center bg-black/70  text-neutral-200  md:text-xl py-3 px-4">
+      Aprovecha los descuentos del <strong>Tuki Friday</strong>
+      <button className="bg-white hover:bg-neutral-200 rounded-md ml-6 mt-3 md:mt-0 py-2 px-4 md:py-1 text-base font-bold text-black">
+        Ver Cupones
+      </button>
+    </div>
+  );
+};
+
+export default Banner;
